6209fca0f37796fd729cd9f7e3e0952322166416
Panatda Phanthavong <panatda.phanthavong@iut-dijon.u-bourgogne.fr>
2026-01-11 01:41:22 +0100

    Ajout Plates3.5 et création objet Engine, Tester l'affichage Home et Template

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
-echo("hello work");
+declare(strict_types=1);
+
+require_once __DIR__ . '/Helpers/Psr4AutoloaderClass.php';
+require_once __DIR__ . '/Vendor/Plates/src/Engine.php';
+
+
+$loader = new Helpers\Psr4AutoloaderClass();
+$loader->register();  //Cela permet de faire comprendre à PhP que cest cette classe qui va gérer l'autoload.
+//ajouter namespace
+$loader->addNamespace('\Helpers', '/Helpers');
+$loader->addNamespace('\League\Plates', '/Vendor/Plates/src');
+
+use League\Plates\Engine;
+$templates = new Engine(__DIR__ . '/Views');
+
+echo $templates->render('home', [
+    'gameName' => 'Genshin Impact'
+]);
+
+
 ?>
