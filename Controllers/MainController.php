bd955d0b009511caf394541bc17827bf8b3b6009
Panatda Phanthavong <panatda.phanthavong@iut-dijon.u-bourgogne.fr>
2026-01-11 18:19:50 +0100

    Ajout Classe MainController

---
+++ Controllers/MainController.php
@@ -0,0 +1,20 @@
+<?php
+namespace Controllers;
+
+use League\Plates\Engine;
+
+class MainController{
+    private Engine $templates;
+
+    public function __construct(Engine $engine)
+    {
+        $this->templates = $engine;
+    }
+
+    public function index() : void
+    {
+        echo $this->templates->render('home', ['gameName' => 'Genshin Impact']);
+    }
+}
+
+?>
--- index.php
+++ index.php
@@ -3,20 +3,27 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/Helpers/Psr4AutoloaderClass.php';
 require_once __DIR__ . '/Vendor/Plates/src/Engine.php';
-
-
+require_once __DIR__ . '/Controllers/MainController.php';
+//charger le namespace
+use League\Plates\Engine;
+use Controllers\MainController;
+//création l'autoloader
 $loader = new Helpers\Psr4AutoloaderClass();
 $loader->register();  //Cela permet de faire comprendre à PhP que cest cette classe qui va gérer l'autoload.
 //ajouter namespace
 $loader->addNamespace('\Helpers', '/Helpers');
 $loader->addNamespace('\League\Plates', '/Vendor/Plates/src');
+$loader->addNamespace('Controllers', '/Controllers');
 
-use League\Plates\Engine;
-$templates = new Engine(__DIR__ . '/Views');
+// Initialisation de l'engine avec le dossier Views qui se trouve les fichiers home.php et template.php
+$engine = new Engine(__DIR__ . '/Views');
 
-echo $templates->render('home', [
-    'gameName' => 'Genshin Impact'
-]);
+// Appel du contrôleur principal
+$controller = new MainController($engine);
+// Appel de la méthode index pour afficher la page
+$controller->index();
+
+
 
 
 ?>
